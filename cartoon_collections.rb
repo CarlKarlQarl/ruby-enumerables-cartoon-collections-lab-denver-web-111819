b7687c9d6e150dd4f6976bbac3e47900957ae8f9
Karl Matthes <karlmatthes@gmail.com>
2019-11-08 19:12:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,9 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index{|dwarf, index| puts "/#{index + 1}. #{dwarf}/"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  call_array = []
+  planeteer_calls.map{|planeteer_calls| call_array.push("#{planeteer_calls.capitalize} + '!'")}
 end
 
 def long_planeteer_calls# code an argument here
